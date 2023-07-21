8d73aa05669916765c23e776ceac0f5b20ce8836
pErfEcto2 <misha.yaro2004@gmail.com>
2023-07-21 10:54:31 +0300

    post changing function added

--- tg_bot/main.py
+++ tg_bot/main.py
@@ -4,6 +4,29 @@ import re
 import psycopg2 as psc
 
 
+def check_data(message, info: list[str]) -> bool:
+    if len(info) != 6:
+        bot.send_message(message.chat.id, "Неправильный формат, нужно ровно 6 строк как в примере\nМожешь написать /help, чтобы увидеть пример")
+        return False
+        
+    if not re.fullmatch("(С )?\d\d(:|\.)\d\d( до \d\d(.|:)\d\d)?", info[0]):
+        bot.send_message(message.chat.id, "Неправильный формат времени, надо вот так:\nC <начальное время> до <конечное время( необязательно )>\nМожешь написать /help, чтобы посмотреть пример")
+        return False
+        
+    if not re.fullmatch("\d\d? \w+ \d{4}", info[1]):
+        bot.send_message(message.chat.id, "Неправильный формат даты, надо вот так:\n <число> <месяц> <год>\nМожешь написать /help, чтобы посмотреть пример")
+        return False
+
+    if not (info[2] and info[3] and info[4]):
+        bot.send_message(message.chat.id, "Надо написать название, описание и место, можешь посмотреть пример, написав /help")
+        return False
+
+    if info[5].lower() not in ["музыка", "сходка", "иное"]:
+        bot.send_message(message.chat.id, "Неправильная категория, можно только что-то из этого: музыка, сходка, иное\nМожешь написать /help, чтобы посмотреть пример")
+        return False
+    return True
+    
+
 def exec_query(query: str) -> "list[tuple[str]]":
     with psc.connect(dbname=config.dbname, user=config.dbuser) as connection:
         with connection.cursor() as cursor:
@@ -11,6 +34,40 @@ def exec_query(query: str) -> "list[tuple[str]]":
             connection.commit()
             res = cursor.fetchall()
     return res
+
+def get_and_check_event_name(message) -> None:
+    names = exec_query("select name from news")
+    if (message.text.lower(), ) not in names:
+        bot.send_message(message.chat.id, "Нет такого мероприятия")
+        return
+    
+    bot.send_message(message.chat.id, "Введи исправленный вариант полностью. Введи /help, чтобы увидеть пример, если не помнишь точно формат записи")
+    bot.register_next_step_handler(message, change_info_and_post)
+
+def change_info_and_post(message) -> None:
+    info = list(map(lambda x: x.strip(), message.text.split("\n")))
+    
+    if not check_data(message, info):
+        return
+    
+    try: 
+        exec_query(f"""update news set \"time\" = '{info[0]}',
+                                         date = '{info[1]}',
+                                         name = '{info[2]}',
+                                         description = '{info[3]}',
+                                         place = '{info[4]}',
+                                         category = '{info[5]}'
+                                         where name = '{message.text.lower()}'""")
+    except psc.ProgrammingError:
+        pass
+
+    bot.send_message(message.chat.id, "Все хоpошо, записал")
+
+    bot.send_message(config.channel_id, f"""ОБНОВЛЕНИЕ В МЕРОПЯТИИ: "{info[2]}"\n
+Описание: {info[3]}\n
+Когда: {'В' if len(info[1]) == 5 else ''}{info[1]}\n
+Где: {info[4]}\n
+Категория: {info[5]}""")
 
 bot = tb.TeleBot(config.token)
 
@@ -29,39 +86,28 @@ def say_hi(message):
 def main(message):
     if message.from_user.username not in config.users:
         return
+    
+    if message.text.lower() == "изменить" and message.from_user.username in config.admins:
+        bot.send_message(message.chat.id, "Напиши название мероприятия")
+        bot.register_next_step_handler(message, get_and_check_event_name)
+        return
 
     info = list(map(lambda x: x.strip(), message.text.split("\n")))
-        
-    if len(info) != 6:
-        bot.send_message(message.chat.id, "Неправильный формат, нужно ровно 6 строк как в примере\nМожешь написать /help, чтобы увидеть пример")
-        return
-        
-    if not re.fullmatch("(С )?\d\d(:|\.)\d\d( до \d\d(.|:)\d\d)?", info[0]):
-        bot.send_message(message.chat.id, "Неправильный формат времени, надо вот так:\nC <начальное время> до <конечное время( необязательно )>\nМожешь написать /help, чтобы посмотреть пример")
-        return
-        
-    if not re.fullmatch("\d\d? \w+ \d{4}", info[1]):
-        bot.send_message(message.chat.id, "Неправильный формат даты, надо вот так:\n <число> <месяц> <год>\nМожешь написать /help, чтобы посмотреть пример")
-        return
 
-    if not (info[2] and info[3] and info[4]):
-        bot.send_message(message.chat.id, "Надо написать название, описание и место, можешь посмотреть пример, написав /help")
-        return
-
-    if info[5].lower() not in ["музыка", "сходка", "иное"]:
-        bot.send_message(message.chat.id, "Неправильная категория, можно только что-то из этого: музыка, сходка, иное\nМожешь написать /help, чтобы посмотреть пример")
+    if not check_data(message, info):
         return
     
     try:
-        exec_query(f"""insert into news (time, date, name, description, place, category) values ('{info[0]}', '{info[1]}', '{info[2]}', '{info[3]}', '{info[4]}', '{info[5]}')""")
+        exec_query(f"""insert into news (time, date, name, description, place, category) 
+                       values ('{info[0].lower()}', '{info[1].lower()}', '{info[2].lower()}', '{info[3].lower()}', '{info[4].lower()}', '{info[5].lower()}')""")
     except psc.ProgrammingError:
         pass
 
     bot.send_message(message.chat.id, "Все хорошо, записал")
-    bot.send_message(config.channel_id, f"""Название: "{info[2]}"
-Описание: {info[3]}
-Когда: {'В' if len(info[1]) == 5 else ''}{info[1]}
-Где: {info[4]}
+    bot.send_message(config.channel_id, f"""Название: "{info[2]}"\n
+Описание: {info[3]}\n
+Когда: {'В' if len(info[1]) == 5 else ''}{info[1]}\n
+Где: {info[4]}\n
 Категория: {info[5]}""")
 
 bot.polling(non_stop=True)

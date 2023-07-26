a485db35d6a53cbb6ff15a2bddb24f9fac241475
pErfEcto2 <misha.yaro2004@gmail.com>
2023-07-26 15:28:57 +0300

    minor changes

--- news_website/news/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-
-def index(req):
-    return HttpResponse("Hello, World!")
-
--- tg_bot/main.py
+++ tg_bot/main.py
@@ -5,8 +5,8 @@ import psycopg2 as psc
 
 
 def check_data(message, info: list[str]) -> bool:
-    if len(info) != 6:
-        bot.send_message(message.chat.id, "Неправильный формат, нужно ровно 6 строк как в примере\nМожешь написать /help, чтобы увидеть пример")
+    if len(info) != 7:
+        bot.send_message(message.chat.id, "Неправильный формат, нужно ровно 7 строк как в примере\nМожешь написать /help, чтобы увидеть пример")
         return False
         
     if not re.fullmatch("(С )?\d\d(:|\.)\d\d( до \d\d(.|:)\d\d)?", info[0]):
@@ -17,8 +17,8 @@ def check_data(message, info: list[str]) -> bool:
         bot.send_message(message.chat.id, "Неправильный формат даты, надо вот так:\n <число> <месяц> <год>\nМожешь написать /help, чтобы посмотреть пример")
         return False
 
-    if not (info[2] and info[3] and info[4]):
-        bot.send_message(message.chat.id, "Надо написать название, описание и место, можешь посмотреть пример, написав /help")
+    if not all(info[2:5] + [info[6]]):
+        bot.send_message(message.chat.id, "Надо написать название, описание, место и стоимость, можешь посмотреть пример, написав /help")
         return False
 
     if info[5].lower() not in ["музыка", "сходка", "иное"]:
@@ -37,7 +37,7 @@ def exec_query(query: str) -> "list[tuple[str]]":
 
 def get_and_check_event_name(message) -> None:
     names = exec_query("select name from news")
-    if (message.text.lower(), ) not in names:
+    if (message.text.lower(), ) not in list(map(lambda x: (x[0].lower(), ), names)):
         bot.send_message(message.chat.id, "Нет такого мероприятия")
         return
     
@@ -50,36 +50,39 @@ def change_info_and_post(message) -> None:
     if not check_data(message, info):
         return
     
-    try: 
+    try:
         exec_query(f"""update news set \"time\" = '{info[0]}',
                                          date = '{info[1]}',
                                          name = '{info[2]}',
                                          description = '{info[3]}',
                                          place = '{info[4]}',
-                                         category = '{info[5]}'
-                                         where name = '{message.text.lower()}'""")
+                                         category = '{info[5]}',
+                                         price = '{info[6]}'
+                                         where name = '{info[2]}'""")
     except psc.ProgrammingError:
         pass
 
     bot.send_message(message.chat.id, "Все хоpошо, записал")
 
     bot.send_message(config.channel_id, f"""ОБНОВЛЕНИЕ В МЕРОПЯТИИ: "{info[2]}"\n
-Описание: {info[3]}\n
-Когда: {'В' if len(info[1]) == 5 else ''}{info[1]}\n
-Где: {info[4]}\n
-Категория: {info[5]}""")
+{info[3]}\n
+{'В ' if len(info[0]) == 5 else ''}{info[0]}
+{info[1]}
+{info[4]}
+{info[6]}
+#{info[5]}""")
 
 bot = tb.TeleBot(config.token)
 
 @bot.message_handler(commands=["start", "help"])
 def say_hi(message):
     bot.send_message(message.chat.id, "Привет\nФормат следующий:")
-    bot.send_message(message.chat.id, "C <начальное время( обязательно )> до <конечное время( необязательно )>\n<число> <месяц( не цифрами )> <год>\nНазвание\nОписание\nМесто\nКатегория")
+    bot.send_message(message.chat.id, "C <начальное время( обязательно )> до <конечное время( необязательно )>\n<число> <месяц( не цифрами )> <год>\nНазвание\nОписание\nМесто\nКатегория\nЦена(если бесплатно, то пиши \"бесплатно\")")
     bot.send_message(message.chat.id, "Название, описание и место писать без переносов строк\nКатегории только такие: музыка, сходки, иное")
     bot.send_message(message.chat.id, "Пример:")
-    bot.send_message(message.chat.id, "С 12:00 до 18:00\n24 ноября 2024\nКрутое событие\nЭто событие будет во истину крутым, все приходите\nУлица Пушкина, 10\nМузыка")
+    bot.send_message(message.chat.id, "С 12:00 до 18:00\n24 ноября 2024\nКрутое событие\nЭто событие будет во истину крутым, все приходите\nУлица Пушкина, 10\nМузыка\n1200 рублей")
     bot.send_message(message.chat.id, "Другой пример:")
-    bot.send_message(message.chat.id, "12.00\n12 ноября 2024\nХорошее мероприятие\nРеально хорошее мероприятие\nТРЦ Солнышко\nИное")
+    bot.send_message(message.chat.id, "12.00\n12 ноября 2024\nХорошее мероприятие\nРеально хорошее мероприятие\nТРЦ Солнышко\nИное\nБесплатно")
 
 
 @bot.message_handler(content_types=["text"])
@@ -98,17 +101,19 @@ def main(message):
         return
     
     try:
-        exec_query(f"""insert into news (time, date, name, description, place, category) 
-                       values ('{info[0].lower()}', '{info[1].lower()}', '{info[2].lower()}', '{info[3].lower()}', '{info[4].lower()}', '{info[5].lower()}')""")
+        exec_query(f"""insert into news (time, date, name, description, place, category, price) 
+                       values ('{info[0]}', '{info[1]}', '{info[2]}', '{info[3]}', '{info[4]}', '{info[5]}', '{info[6]}')""")
     except psc.ProgrammingError:
         pass
 
     bot.send_message(message.chat.id, "Все хорошо, записал")
-    bot.send_message(config.channel_id, f"""Название: "{info[2]}"\n
-Описание: {info[3]}\n
-Когда: {'В' if len(info[1]) == 5 else ''}{info[1]}\n
-Где: {info[4]}\n
-Категория: {info[5]}""")
+    bot.send_message(config.channel_id, f""""{info[2]}"\n
+{info[3]}\n
+{'В ' if len(info[0]) == 5 else ''}{info[0]}
+{info[1]}
+{info[4]}
+{info[6]}
+#{info[5]}""")
 
 bot.polling(non_stop=True)
 
